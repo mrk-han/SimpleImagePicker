25dc47bc5efa6945365d714ab76389d2956bfaf5
mrk-han <mark.han@willowtreeapps.com>
2022-02-19 13:01:21 -0500

    add uialertcontroller + alertaction, comment out picker

--- SimpleImagePicker/ViewController.swift
+++ SimpleImagePicker/ViewController.swift
@@ -18,9 +18,36 @@ class ViewController: UIViewController {
     
     @IBAction func experiment(){
         
-        let nextController = UIImagePickerController()
         
-        present(nextController, animated: true, completion: nil)
+        /*
+         Creating UIAlertController and presenting it
+         */
+         
+        let alertController = UIAlertController(title: "test", message: "testtest", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "ok", style: .default) {
+            action in self.dismiss(animated: true, completion: nil)
+        }
+
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+        
+        /*
+         Creating UIActivityViewController and presenting it
+         */
+//        let image = UIImage()
+//        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+//
+//        present(controller, animated: true, completion: nil)
+        
+        
+        
+        /*
+         Creating UIImagePickerController and Presenting it
+         */
+//        let nextController = UIImagePickerController()
+//
+//        present(nextController, animated: true, completion: nil)
     }
 
 
